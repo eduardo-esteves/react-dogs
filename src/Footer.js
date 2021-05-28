f786de8e8a2d89129b20b59d29930ba8c2276806
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2021-05-28 20:55:44 -0300

    Criado Header e Footer componentes

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+
+const Footer = () => {
+  return(
+  <div className={styles.footer}>Footer Component</div>
+  )
+}
+
+
+export default Footer
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+  <div className={styles.header}>Header Component</div>
+  )
+}
+
+
+export default Header
