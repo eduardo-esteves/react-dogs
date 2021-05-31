191a10a4083177468c8432fa24b0db8b618730c4
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2021-05-31 18:07:04 -0300

    Criado novo componente Home.js e seu module.css adicionado o componente BrowserRoute e criado nova rota home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './Footer'
 import Header from './Header'
+import { Home } from './Home'
 
 function App() {
   return (
     <div className="App">
       <h2>Adicionando meus dois componentes bases Header e Footer</h2>
-      <Header />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
