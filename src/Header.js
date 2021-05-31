0cccbde3e86528449d8161867623278a5636ea90
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2021-05-31 19:51:37 -0300

    Feito a estilizacao do componente Header e importado o icone dogs.svg como um componente react

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,15 @@
 import styles from './Header.module.css'
 import { Link } from 'react-router-dom'
+import { ReactComponent as Dogs} from './assets/dogs.svg'
 
 const Header = () => {
   return (
-  <div className={styles.header}>
-    <nav className="container">
-      <Link to="/">Home</Link>
-      <Link to="/login">Login / Criar</Link>
+  <header className={styles.header}>
+    <nav className={`container ${styles.nav}`}>
+      <Link className={styles.logo} to="/" aria-label="Dogs - Home"><Dogs /></Link>
+      <Link className={styles.login} to="/login">Login / Criar</Link>
     </nav>
-  </div>
+  </header>
   )
 }
 
